df568d8442aa6fbe6d2a836932e677a2fddf9e90
Olivia Marcolini <oliviamonteiro2406@gmail.com>
2025-07-14 11:00:29 -0300

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Olivia Monteiro on 7/14/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
